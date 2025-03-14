883889cba1b194edf5bd4b917d9a101a15a430b7
BayborodinAlexandr <albayborodin@yandex.ru>
2025-03-14 21:56:03 +0300

    Добавлены файлы по домашней работе

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\News; 
+use App\Events\NewsHidden; 
+
+Route::get('/news/create-test', function () {
+    $news = new News();
+    $news->title = 'Test news title';
+    $news->body = 'Test news body';
+    $news->save();
+    return $news;
+});
+
+Route::get('/news/{id}/hide', function ($id) {
+    $news = News::findOrFail($id);
+    $news->hidden = true;
+    $news->save();
+    event(new NewsHidden($news));
+    return 'News hidden';
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
